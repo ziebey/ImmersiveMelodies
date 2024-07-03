b33830374bcb1acd7a28bdd7c852f7dbe8c5477f
Luke100000 <jonas.peche@aon.at>
2024-07-03 21:17:05 +0200

    updated gradle to fix refmap issues #66

--- common/src/main/java/immersive_melodies/client/gui/ImmersiveMelodiesScreen.java
+++ common/src/main/java/immersive_melodies/client/gui/ImmersiveMelodiesScreen.java
@@ -25,7 +25,6 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
 
-import javax.annotation.Nullable;
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -53,7 +52,6 @@ public class ImmersiveMelodiesScreen extends Screen {
         this.search.setSuggestion(null);
     }
 
-    @Nullable
     private Identifier selected = null;
 
     public ImmersiveMelodiesScreen() {
