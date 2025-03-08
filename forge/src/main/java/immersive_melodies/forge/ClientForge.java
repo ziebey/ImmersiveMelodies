5b18ab9cb7826c77fcde7c2cf97f4287819f9c53
Luke100000 <jonas.peche@aon.at>
2025-03-08 11:43:02 +0100

    maybe fixed #74

--- forge/src/main/java/immersive_melodies/forge/ClientForge.java
+++ forge/src/main/java/immersive_melodies/forge/ClientForge.java
@@ -10,8 +10,12 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid = Common.MOD_ID, value = Dist.CLIENT, bus = Bus.MOD)
 public final class ClientForge {
+    static {
+        RegistrationImpl.bootstrap();
+    }
+
     @SubscribeEvent
     public static void setup(FMLClientSetupEvent event) {
-        RegistrationImpl.bootstrap();
+        // NOP
     }
 }
